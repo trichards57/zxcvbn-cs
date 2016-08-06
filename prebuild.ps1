811008bf341c0597046d20e1d4898a9287325529
Tony Richards <trichards57@gmail.com>
2016-08-06 13:04:00 +0100

    Fixed a path in the prebuild script.

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,5 +1,5 @@
 Write-Host "Patching project.json version..."
-(Get-Content $PSScriptRoot\zxcvbn-core\project.ci.json).replace('$version$', $Env:APPVEYOR_BUILD_VERSION) | Set-Content $PSScriptRoot\Lorem.DNX.NET\project.json
+(Get-Content $PSScriptRoot\zxcvbn-core\project.ci.json).replace('$version$', $Env:APPVEYOR_BUILD_VERSION) | Set-Content $PSScriptRoot\zxcvbn-core\project.json
 Write-Host "Updated project.json to use version $($Env:APPVEYOR_BUILD_VERSION)"
 
 dotnet restore
