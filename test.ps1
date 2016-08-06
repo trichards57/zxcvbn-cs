0e6c25781237ec08a90ed94742cb8d412b84256c
Tony Richards <trichards57@gmail.com>
2016-08-06 13:41:55 +0100

    Updated testing and corrected embedded resources.

---
+++ test.ps1
@@ -0,0 +1,1 @@
+dotnet test zxcvbn-core-test
--- zxcvbn-core/Matcher/DictionaryMatcher.cs
+++ zxcvbn-core/Matcher/DictionaryMatcher.cs
@@ -118,7 +118,7 @@ namespace Zxcvbn.Matcher
         {
             // Look first to wordlists embedded in assembly (i.e. default dictionaries) otherwise treat as file path
 
-            var lines = Utility.GetEmbeddedResourceLines("Zxcvbn.Dictionaries.{0}".F(wordListFile)) ?? File.ReadAllLines(wordListFile);
+            var lines = Utility.GetEmbeddedResourceLines("zxcvbn-core.Dictionaries.{0}".F(wordListFile)) ?? File.ReadAllLines(wordListFile);
 
             return BuildRankedDictionary(lines);
         }
