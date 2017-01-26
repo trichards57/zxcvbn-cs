ac29d74d352c8c928cd3958f66ba47b1acf47bc5
mathieuchevalier <chevalj@hotmail.com>
2017-01-25 20:01:30 -0500

    Added Suggestion and warning enums

--- Result.cs
+++ Result.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +7,175 @@ using System.Text;
 namespace Zxcvbn
 {
     /// <summary>
+    /// SWarning associated with the password analysis
+    /// </summary>
+    public enum Warning
+    {
+        /// <summary>
+        /// Empty string
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Straight rows of keys are easy to guess
+        /// </summary>
+        StraightRow,
+
+        /// <summary>
+        /// Short keyboard patterns are easy to guess
+        /// </summary>
+        ShortKeyboardPatterns,
+
+        /// <summary>
+        /// Repeats like "aaa" are easy to guess
+        /// </summary>
+        RepeatsLikeAaaEasy,
+
+        /// <summary>
+        /// Repeats like "abcabcabc" are only slightly harder to guess than "abc"
+        /// </summary>
+        RepeatsLikeAbcSlighterHarder,
+
+        /// <summary>
+        /// Sequences like abc or 6543 are easy to guess
+        /// </summary>
+        SequenceAbcEasy,
+
+        /// <summary>
+        /// Recent years are easy to guess
+        /// </summary>
+        RecentYearsEasy,
+
+        /// <summary>
+        ///  Dates are often easy to guess
+        /// </summary>
+        DatesEasy,
+
+        /// <summary>
+        ///  This is a top-10 common password
+        /// </summary>
+        Top10Passwords,
+
+        /// <summary>
+        /// This is a top-100 common password
+        /// </summary>
+        Top100Passwords,
+
+        /// <summary>
+        /// This is a very common password
+        /// </summary>
+        CommonPasswords,
+
+        /// <summary>
+        /// This is similar to a commonly used password
+        /// </summary>
+        SimilarCommonPasswords,
+
+        /// <summary>
+        /// A word by itself is easy to guess
+        /// </summary>
+        WordEasy,
+
+        /// <summary>
+        /// Names and surnames by themselves are easy to guess
+        /// </summary>
+        NameSurnamesEasy,
+
+        /// <summary>
+        /// Common names and surnames are easy to guess
+        /// </summary>
+        CommonNameSurnamesEasy,
+
+        /// <summary>
+        ///  Empty String
+        /// </summary>
+        Empty,
+
+    }
+
+    /// <summary>
+    /// Suggestion on how to improve the password base on zxcvbn's password analysis
+    /// </summary>
+    public enum Suggestion
+    {
+        /// <summary>
+        ///  Use a few words, avoid common phrases
+        ///  No need for symbols, digits, or uppercase letters
+        /// </summary>
+        Default,
+
+        /// <summary>
+        ///  Add another word or two. Uncommon words are better.
+        /// </summary>
+        AddAnotherWordOrTwo,
+
+        /// <summary>
+        ///  Use a longer keyboard pattern with more turns
+        /// </summary>
+        UseLongerKeyboardPattern,
+
+        /// <summary>
+        ///  Avoid repeated words and characters
+        /// </summary>
+        AvoidRepeatedWordsAndChars,
+
+        /// <summary>
+        ///  Avoid sequences  
+        /// </summary>
+        AvoidSequences,
+
+        /// <summary>
+        ///  Avoid recent years
+        ///  Avoid years that are associated with you
+        /// </summary>
+        AvoidYearsAssociatedYou,
+
+        /// <summary>
+        ///  Avoid dates and years that are associated with you 
+        /// </summary>
+        AvoidDatesYearsAssociatedYou,
+
+        /// <summary>
+        ///  Capitalization doesn't help very much  
+        /// </summary>
+        CapsDontHelp,
+
+        /// <summary>
+        /// All-uppercase is almost as easy to guess as all-lowercase   
+        /// </summary>
+        AllCapsEasy,
+
+        /// <summary>
+        /// Reversed words aren't much harder to guess   
+        /// </summary>
+        ReversedWordEasy,
+
+        /// <summary>
+        ///  Predictable substitutions like '@' instead of 'a' don't help very much  
+        /// </summary>
+        PredictableSubstitutionsEasy,
+
+        /// <summary>
+        ///  Empty String
+        /// </summary>
+        Empty,
+
+    }
+
+
+
+
+
+    /// <summary>
     /// The results of zxcvbn's password analysis
     /// </summary>
     public class Result
     {
+        /// <summary>
+        /// Result constructor initialize Suggestion list.
+        /// </summary>
+        public Result() { suggestions = new List<Suggestion>(); }
+
         /// <summary>
         /// A calculated estimate of how many bits of entropy the password covers, rounded to three decimal places.
         /// </summary>
@@ -37,7 +202,7 @@ namespace Zxcvbn
         /// Useful for implementing a strength meter
         /// </summary>
         public int Score { get; set; }
-        
+
         /// <summary>
         /// The sequence of matches that were used to create the entropy calculation
         /// </summary>
@@ -47,6 +212,18 @@ namespace Zxcvbn
         /// The password that was used to generate these results
         /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// Warning on this password
+        /// </summary>
+        public Warning  warning {get; set;}
+
+        /// <summary>
+        /// Suggestion on how to improve the password
+        /// </summary>
+        public List<Suggestion> suggestions { get; set; }
+
+
     }
 
     /// <summary>
