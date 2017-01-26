ba9184f37e841516172ab3d1d72a35c60a08699e
mathieuchevalier <chevalj@hotmail.com>
2017-01-25 20:02:24 -0500

    Added English translation for suggestion and warnings

--- Utility.cs
+++ Utility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -215,5 +215,417 @@ namespace Zxcvbn
 
             return lines;
         }
+
+        /// <summary>
+        /// Get a translated string of the Warning
+        /// </summary>
+        /// <param name="warning">Warning enum to get the string from</param>
+        /// <param name="translation">Language in which to return the string to. Default is English.</param>
+        /// <returns>Warning string in the right language</returns>
+        public static string GetWarning(Warning warning, Translation translation = Translation.English)
+        {
+            string translated;
+
+            switch (warning)
+            {
+                case Warning.StraightRow:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Straight rows of keys are easy to guess";
+                            break;
+                    }
+                    break;
+
+                case Warning.ShortKeyboardPatterns:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Short keyboard patterns are easy to guess";
+                            break;
+                    }
+                    break;
+
+                case Warning.RepeatsLikeAaaEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Repeats like \"aaa\" are easy to guess";
+                            break;
+                    }
+                    break;
+
+                case Warning.RepeatsLikeAbcSlighterHarder:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Repeats like \"abcabcabc\" are only slightly harder to guess than \"abc\"";
+                            break;
+                    }
+                    break;
+                case Warning.SequenceAbcEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Sequences like abc or 6543 are easy to guess";
+                            break;
+                    }
+                    break;
+                case Warning.RecentYearsEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Recent years are easy to guess";
+                            break;
+                    }
+                    break;
+                case Warning.DatesEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Dates are often easy to guess";
+                            break;
+                    }
+                    break;
+                case Warning.Top10Passwords:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "This is a top-10 common password";
+                            break;
+                    }
+                    break;
+                case Warning.Top100Passwords:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "This is a top-100 common password";
+                            break;
+                    }
+                    break;
+                case Warning.CommonPasswords:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "This is a very common password";
+                            break;
+                    }
+                    break;
+                case Warning.SimilarCommonPasswords:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "This is similar to a commonly used password";
+                            break;
+                    }
+                    break;
+                case Warning.WordEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "A word by itself is easy to guess";
+                            break;
+                    }
+                    break;
+                case Warning.NameSurnamesEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Names and surnames by themselves are easy to guess";
+                            break;
+                    }
+                    break;
+                case Warning.CommonNameSurnamesEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Common names and surnames are easy to guess";
+                            break;
+                    }
+                    break;
+
+                case Warning.Empty:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "";
+                            break;
+                    }
+                    break;
+
+                default:
+                    translated = "";
+                    break;
+            }
+            return translated;
+        }
+
+        /// <summary>
+        /// Get a translated string of the Warning
+        /// </summary>
+        /// <param name="suggestion">Suggestion enum to get the string from</param>
+        /// <param name="translation">Language in which to return the string to. Default is English.</param>
+        /// <returns>Suggestion string in the right language</returns>
+        public static string GetSuggestion(Suggestion suggestion, Translation translation = Translation.English)
+        {
+            string translated;
+
+            switch (suggestion)
+            {
+                case Suggestion.AddAnotherWordOrTwo:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Add another word or two. Uncommon words are better.";
+                            break;
+                    }
+                    break;
+
+                case Suggestion.UseLongerKeyboardPattern:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Use a longer keyboard pattern with more turns";
+                            break;
+                    }
+                    break;
+
+                case Suggestion.AvoidRepeatedWordsAndChars:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Avoid repeated words and characters";
+                            break;
+                    }
+                    break;
+
+                case Suggestion.AvoidSequences:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Avoid sequences";
+                            break;
+                    }
+                    break;
+                case Suggestion.AvoidYearsAssociatedYou:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Avoid recent years \n Avoid years that are associated with you";
+                            break;
+                    }
+                    break;
+                case Suggestion.AvoidDatesYearsAssociatedYou:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Avoid dates and years that are associated with you";
+                            break;
+                    }
+                    break;
+                case Suggestion.CapsDontHelp:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Capitalization doesn't help very much";
+                            break;
+                    }
+                    break;
+                case Suggestion.AllCapsEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "All-uppercase is almost as easy to guess as all-lowercase";
+                            break;
+                    }
+                    break;
+                case Suggestion.ReversedWordEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Reversed words aren't much harder to guess";
+                            break;
+                    }
+                    break;
+                case Suggestion.PredictableSubstitutionsEasy:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "Predictable substitutions like '@' instead of 'a' don't help very much";
+                            break;
+                    }
+                    break;
+
+                case Suggestion.Empty:
+                    switch (translation)
+                    {
+                        case Translation.German:
+                            translated = "";
+                            break;
+                        case Translation.France:
+                            translated = "";
+                            break;
+                        default:
+                            translated = "";
+                            break;
+                    }
+                    break;
+                default:
+                    translated = "Use a few words, avoid common phrases \n No need for symbols, digits, or uppercase letters";
+                    break;
+            }
+            return translated;
+        }
+
     }
 }
