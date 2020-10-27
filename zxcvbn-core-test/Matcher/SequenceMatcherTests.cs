f74ac57ab211f01a4eed604e5a5c8d9f6c1bdb22
Tony Richards <trichards57@outlook.com>
2020-10-27 09:29:52 +0000

    Made the Sequence Matcher match the typescript tests.

--- zxcvbn-core-test/Matcher/SequenceMatcherTests.cs
+++ zxcvbn-core-test/Matcher/SequenceMatcherTests.cs
@@ -11,7 +11,7 @@ namespace Zxcvbn.Tests.Matcher
         private readonly SequenceMatcher _matcher = new SequenceMatcher();
 
         [Theory, InlineData(""), InlineData("a"), InlineData("1")]
-        public void DoesntMatchShortSequences(string password)
+        public void DoesntMatchVeryEmptyOrOneLengthSequences(string password)
         {
             var res = _matcher.MatchPassword(password);
             res.Should().BeEmpty();
@@ -26,60 +26,77 @@ namespace Zxcvbn.Tests.Matcher
             var i = prefix.Length;
             var j = i + pattern.Length - 1;
 
-            var res = _matcher.MatchPassword(password).OfType<SequenceMatch>().ToList();
+            var result = _matcher.MatchPassword(password).OfType<SequenceMatch>().ToList();
 
-            res.Should().HaveCount(1);
+            var expected = new[] { new SequenceMatch
+            {
+                Ascending = false,
+                i=i,
+                j=j,
+                Pattern = "sequence",
+                SequenceName = "lower",
+                SequenceSpace = 26,
+                Token = pattern
+            } };
+            result.Should().BeEquivalentTo(expected);
+        }
 
-            res[0].Pattern.Should().Be("sequence");
-            res[0].Token.Should().Be(pattern);
-            res[0].i.Should().Be(i);
-            res[0].j.Should().Be(j);
-            res[0].Ascending.Should().Be(false);
+        [Theory,
+         InlineData("ABC", "upper", true, 26), InlineData("CBA", "upper", false, 26), InlineData("PQR", "upper", true, 26), InlineData("RQP", "upper", false, 26), InlineData("XYZ", "upper", true, 26), InlineData("ZYX", "upper", false, 26),
+         InlineData("abcd", "lower", true, 26), InlineData("dcba", "lower", false, 26), InlineData("jihg", "lower", false, 26), InlineData("wxyz", "lower", true, 26), InlineData("zxvt", "lower", false, 26),
+         InlineData("0369", "digits", true, 10), InlineData("97531", "digits", false, 10)
+        ]
+        public void MatchesGeneralSequence(string password, string name, bool ascending, int space)
+        {
+            var result = _matcher.MatchPassword(password).OfType<SequenceMatch>().ToList();
+
+            var expected = new[] { new SequenceMatch
+            {
+                Ascending = ascending,
+                i=0,
+                j=password.Length - 1,
+                Pattern = "sequence",
+                SequenceName = name,
+                SequenceSpace = space,
+                Token = password
+            } };
+            result.Should().BeEquivalentTo(expected);
         }
 
         [Fact]
         public void MatchesOverlappingPatterns()
         {
-            var res = _matcher.MatchPassword("abcbabc").OfType<SequenceMatch>().ToList();
+            var result = _matcher.MatchPassword("abcbabc").OfType<SequenceMatch>().ToList();
 
-            res.Should().HaveCount(3);
-
-            res[0].Pattern.Should().Be("sequence");
-            res[0].Token.Should().Be("abc");
-            res[0].i.Should().Be(0);
-            res[0].j.Should().Be(2);
-            res[0].Ascending.Should().Be(true);
-
-            res[1].Pattern.Should().Be("sequence");
-            res[1].Token.Should().Be("cba");
-            res[1].i.Should().Be(2);
-            res[1].j.Should().Be(4);
-            res[1].Ascending.Should().Be(false);
-
-            res[2].Pattern.Should().Be("sequence");
-            res[2].Token.Should().Be("abc");
-            res[2].i.Should().Be(4);
-            res[2].j.Should().Be(6);
-            res[2].Ascending.Should().Be(true);
-        }
-
-        [Theory,
-         InlineData("ABC", "upper", true), InlineData("CBA", "upper", false), InlineData("PQR", "upper", true), InlineData("RQP", "upper", false), InlineData("XYZ", "upper", true), InlineData("ZYX", "upper", false),
-         InlineData("abcd", "lower", true), InlineData("dcba", "lower", false), InlineData("jihg", "lower", false), InlineData("wxyz", "lower", true), InlineData("zxvt", "lower", false),
-         InlineData("0369", "digits", true), InlineData("97531", "digits", false)
-        ]
-        public void MatchesSpecificSequence(string password, string name, bool ascending)
-        {
-            var res = _matcher.MatchPassword(password).OfType<SequenceMatch>().ToList();
-
-            res.Should().HaveCount(1);
-
-            res[0].Pattern.Should().Be("sequence");
-            res[0].Token.Should().Be(password);
-            res[0].i.Should().Be(0);
-            res[0].j.Should().Be(password.Length - 1);
-            res[0].Ascending.Should().Be(ascending);
-            res[0].SequenceName = name;
+            var expected = new[] { new SequenceMatch
+            {
+                Ascending = true,
+                i = 0,
+                j = 2,
+                Pattern = "sequence",
+                SequenceName = "lower",
+                SequenceSpace = 26,
+                Token = "abc"
+            },
+            new SequenceMatch{
+                Ascending = false,
+                i = 2,
+                j = 4,
+                Pattern = "sequence",
+                SequenceName = "lower",
+                SequenceSpace = 26,
+                Token = "cba"
+            },
+            new SequenceMatch{
+                Ascending = true,
+                i = 4,
+                j = 6,
+                Pattern = "sequence",
+                SequenceName = "lower",
+                SequenceSpace = 26,
+                Token = "abc"
+            }};
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
