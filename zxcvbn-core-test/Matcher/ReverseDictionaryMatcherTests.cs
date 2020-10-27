a8ab15eaf5c18a3afd0b8e14fb3a14337230b2a9
Tony Richards <trichards57@outlook.com>
2020-10-27 09:20:39 +0000

    Made the reverse dictionary matcher tests.
    Also excludes the TestResults from the test project.

--- zxcvbn-core-test/Matcher/ReverseDictionaryMatcherTests.cs
+++ zxcvbn-core-test/Matcher/ReverseDictionaryMatcherTests.cs
@@ -8,30 +8,37 @@ namespace Zxcvbn.Tests.Matcher
 {
     public class ReverseDictionaryMatcherTests
     {
-        private readonly ReverseDictionaryMatcher _matcher = new ReverseDictionaryMatcher("d3", "test_dictionary_3.txt");
+        private readonly ReverseDictionaryMatcher _matcher = new ReverseDictionaryMatcher("d1", "test_dictionary_3.txt");
 
         [Fact]
         public void MatchesAganstReversedWords()
         {
             var result = _matcher.MatchPassword("0123456789").OfType<DictionaryMatch>().ToList();
 
-            result.Should().HaveCount(2);
-
-            result[0].Token.Should().Be("123");
-            result[0].MatchedWord.Should().Be("321");
-            result[0].Rank.Should().Be(2);
-            result[0].i.Should().Be(1);
-            result[0].j.Should().Be(3);
-            result[0].Reversed.Should().BeTrue();
-            result[0].DictionaryName.Should().Be("d3");
-
-            result[1].Token.Should().Be("456");
-            result[1].MatchedWord.Should().Be("654");
-            result[1].Rank.Should().Be(4);
-            result[1].i.Should().Be(4);
-            result[1].j.Should().Be(6);
-            result[1].Reversed.Should().BeTrue();
-            result[1].DictionaryName.Should().Be("d3");
+            var expected = new[] { new DictionaryMatch
+            {
+                Pattern="dictionary",
+                DictionaryName = "d1",
+                i = 1,
+                j = 3,
+                MatchedWord = "321",
+                Rank = 2,
+                Reversed = true,
+                L33t = false,
+                Token = "123"
+            } ,
+                new DictionaryMatch{
+                Pattern="dictionary",
+                DictionaryName = "d1",
+                i = 4,
+                j = 6,
+                MatchedWord = "654",
+                Rank = 4,
+                Reversed = true,
+                L33t = false,
+                Token = "456"
+            }};
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
